62909082dd25d72318bdb99c6cfb1e10d31edab3
danilo-scataloni <danilo.scataloni@amlabs.com.br>
2025-07-23 11:56:51 -0300

    feat: add color definitions for DeepBlack, LightGray, and BorderGray

---
+++ composeApp/src/commonMain/kotlin/org/liftking/core/utils/Colors.kt
@@ -0,0 +1,10 @@
+package org.liftking.core.utils
+
+import androidx.compose.ui.graphics.Color
+
+
+val DeepBlack: Color = Color(0x0F0F0F)
+val LightGray: Color = Color(0x1A1A1A)
+
+val BorderGray: Color = Color(0x333)
+val BorderGray2: Color = Color(0xDDDDDD)
